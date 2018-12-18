cb28cecd3a059588676d1ffee498a80e74c605a6
dev1 <3331866906@qq.com>
2018-12-18 11:06:17 +0800

    spring基础研究

---
+++ gupaoedu-vip-spring-base/src/test/java/gupaoedu/vip/spring/base/UnitTest.java
@@ -0,0 +1,14 @@
+package gupaoedu.vip.spring.base;
+
+import gupaoedu.vip.spring.base.pojo.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UnitTest {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = (Person)applicationContext.getBean("person");
+        person.setName("zhangsan");
+        System.out.println(person.getName());
+    }
+}
